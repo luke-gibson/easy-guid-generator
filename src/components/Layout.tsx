3d7da2bcd6f0d08f88d2ba1ffa7f2a755b13b2ba
luke-gibson <hello@lukegibsondesign.co.uk>
2025-05-16 11:18:04 +0100

    Update dependencies and refactor Layout component structure

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -6,15 +6,9 @@ interface LayoutProps {
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
-    <div className="min-h-screen bg-[#0D162B] text-white flex flex-col items-center justify-center">
-      <header className="w-full p-4 bg-[#1A2238] text-center">
-        <h1 className="text-2xl font-bold">EasyGUID Generator</h1>
-      </header>
-      <main className="w-full max-w-2xl p-4">{children}</main>
-      <footer className="w-full p-4 bg-[#1A2238] text-center">
-        Powered by Oval Creative
-      </footer>
-    </div>
+      <main className="bg-slate-800 bg-gradient-to-r from-slate-800 to-slate-900 min-h-dvh w-screen flex flex-col items-center justify-center">
+        {children}
+      </main>
   );
 };
 
